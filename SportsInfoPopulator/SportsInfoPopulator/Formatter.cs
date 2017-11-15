b63e49db16f1f065072652a727e010f71465d01e
Eric Pelletier <pelletier.dev93@gmail.com>
2017-11-15 18:56:38 -0500

    Setup

---
+++ SportsInfoPopulator/SportsInfoPopulator/Formatter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace SportsInfoPopulator
+{
+    public class Formatter
+    {
+        
+    }
+}
---
+++ SportsInfoPopulator/SportsInfoPopulator/SportRadarService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace SportsInfoPopulator
+{
+    class SportRadarService
+    {
+        public async Task<JObject> GetSportsData(string formatter, CancellationToken token)
+        {
+            var client = new HttpClient();
+
+            var r = await client.GetAsync($"http://api.sportradar.us/nfl-ot2/seasontd/2017/standings.json?api_key=kprbs6drnp6w3n5um2rh3wc6", token);
+
+            var o = JObject.Parse(await r.Content.ReadAsStringAsync());
+
+            switch (formatter) {
+                case ""
+
+
+            }
+                
+
+
+
+
+            return o;
+        }
+    }
+}
